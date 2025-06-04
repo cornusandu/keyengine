0518272c2ff92a1953fc3a42ad3aa4b3505b4840
cornusandu <cornusandu@hotmail.com>
2025-06-04 19:03:15 +0300

    v1.0.0

---
+++ keyengine/console.py
@@ -0,0 +1,83 @@
+from threading import RLock, Thread, Event
+from . import keydetection as kd
+import keyboard
+import time
+import os
+import sys
+import colorama
+from typing import Any
+
+class Console:
+    def __init__(self):
+        colorama.init()
+        self.outputs = []
+        self.stdin = ''
+        self.t = Thread(target = self._refreshloop)
+        self.t.start()
+        self.lock = RLock()
+        self.s = Event()
+
+    def print(self, *values: str, sep: str = ' ', end: str = '\n'):
+        self.lock.acquire()
+
+        self.outputs.append(('out', sep.join(values) + end))
+
+        self.lock.release()
+
+    def input(self, prompt: str) -> str:
+        kd.start_input(True)
+        while not keyboard.read_event().name != 'enter':
+            with self.lock:
+                self.stdin = prompt + kd.INPUT
+            time.sleep(0.05)
+        val = kd.end_input()
+        self.lock.acquire()
+        self.stdin = ''
+        self.print(str(prompt) + str(val))
+        self.lock.release()
+        return val
+
+    def error(self, err: str):
+        self.lock.acquire()
+        self.outputs.append(('error', err))
+        self.lock.release()
+
+    def log(self, content: str, values: dict[str, Any] | None = None):
+        self.lock.acquire()
+        self.outputs.append(('log', f"[LOG] {content}"))
+        if values:
+            for k, v in values.items():
+                self.outputs.append(('log', f"  |  `{k}` = `{v}`"))
+        self.lock.release()
+
+    def _refreshloop(self):
+        while True:
+            time.sleep(0.05)
+            if self.s.is_set(): return
+            self._update()
+
+    def _update(self):
+        self.lock.acquire()
+        os.system('cls || clear')
+        if len(self.outputs) > 100:
+            self.outputs = self.outputs[69:] # Stop the outputs from being longer than 100.
+        for (t, v) in self.outputs:
+            if t == 'out':
+                sys.stdout.write(f'{v}')
+            elif t == 'error':
+                sys.stderr.write(f'{v}\n')
+            elif t == 'log':
+                sys.stderr.write(f'{v}\n')
+            else:
+                sys.stdout.write(f'{v}\n')
+
+        sys.stdout.write(f"\n{colorama.Style.BRIGHT}{self.stdin}{colorama.Style.RESET_ALL}")
+        sys.stdout.flush()
+        self.lock.release()
+
+    def stop(self):
+        del self.outputs
+        self.s.set()
+
+    def __del__(self):
+        self.stop()
